0eac6bf6d3b8962e14ebf0a1dfda19025dcbab84
Arijesamujaa <arijesamujaa@gmail.com>
2024-11-21 12:53:12 +0100

    new commits

--- museum project/js/upcomingEvents.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const carouselElement = document.querySelector("#carouselExample");
-    const carousel = new bootstrap.Carousel(carouselElement, {
-        interval: 3000, 
-        ride: "carousel", 
-        pause: false,     
-    });
-});
